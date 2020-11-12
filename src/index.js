5d4e6ee1a9a06a9e67002e7581b924431122c81c
Shuyoou <zane_you@foxmail.com>
2020-11-13 07:25:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ require('./index.css');
 
 const show = require('./show.js');
 
-show('Webpack');
+show('Webpack!');
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    show('Webpack!');
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    publicPath: '/',
+    filename: '[name]_[contenthash:8].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    contentBase: './dist',
+    hot: true,
   },
 
   module: {
@@ -14,10 +23,22 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
     ],
   },
 
-  plugins: [new MiniCssExtractPlugin({
-    filename: `[name]_[contenthash:8].css`
-  })],
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
+    new MiniCssExtractPlugin({
+      filename: `[name]_[contenthash:8].css`
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    })
+  ],
 };
